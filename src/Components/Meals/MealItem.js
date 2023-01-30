0dc504643d5d4d5625a96cb3bd37bbc37df84fda
shelter1120 <shelterk1120>
2023-01-30 17:49:09 +0530

    adding form and making input components.

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "../Meals/MealItem.module.css"
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
     const price = `'$'${props.price.toFixed(2)}`
@@ -11,7 +12,9 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
 
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
---
+++ src/Components/Meals/MealItemForm.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+import classes from "../Meals/MealItemForm.module.css"
+import Input from '../UI/Input'
+const MealItemForm = () => {
+  return (
+        <form className={classes.form}>
+            
+        <Input label='Amount' input={{
+            id:'amount',
+            type:'number',
+            min:'1',
+            max:'5',
+            defaultValue:'1'
+
+        }}/>
+        <button>+ Add</button>
+        </form>
+  )
+}
+
+export default MealItemForm
